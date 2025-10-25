064dbdf1089beaaf3f561c42091cbe1c7b3e5401
Saeidparvaz30-commits <saeidparvaz30@gmail.com>
2025-10-25 14:35:14 +0200

    Fix post list padding

--- src/components/Category/PostList.tsx
+++ src/components/Category/PostList.tsx
@@ -64,7 +64,7 @@ export default function PostList({
       variants={list}
       initial="hidden"
       animate="show"
-      className={wrapperClass}
+      className={`${wrapperClass} pb-8`}
     >
       {posts?.map((post, i) => {
         const isRead = visited[post._id];
